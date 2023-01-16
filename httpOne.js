0c0a70fe72d8d037bc787c4836890253356d246d
Josphat karanja <josphatranja243@gmail.com>
2023-01-16 14:22:23 +0300

    HTTP Express Server

---
+++ httpOne.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    console.log(req.url);
+    const url = req.url;
+    if (url === '/') {
+        res.writeHead(200,
+            { 'content-type': 'text/html' },
+        )
+        res.write('<h1>Home Page</h1>');
+        res.end()
+    }
+    else if (url === '/about') {
+        res.writeHead(200,
+            { 'content-type': 'text/html' },
+        )
+        res.write('<h1>About Page</h1>');
+        res.end()
+
+    }
+    else {
+        res.writeHead(
+            404,
+            { 'content-type': 'text/html' }
+        )
+        res.write(
+            '<h1>Page not found</h1>'
+        )
+        res.end()
+
+    }
+
+})
+server.listen(5000, () => {
+    console.log("Server Listening on port 5000....");
+})
