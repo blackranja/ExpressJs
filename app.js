1304d393c159913b097e86b0d9bfa6cf2f5b475b
Josphat karanja <josphatranja243@gmail.com>
2023-01-16 18:49:54 +0300

    Express Static Assets

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+//SETUP STATIC AND MIDDLEWARE(static assets)
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navBar/index.html'))
+//app.get('/', (req, res) => {
+//1.Send index.html
+//res.sendFile(path.resolve(__dirname, './navBar/index.html'))
+//2.Just dump index.html in the static assests(./public folder)
 
-})
+//})
+//3.SSR--Server-Side-Rendering
 
 app.all('*', (req, res) => {
     res.status(404)
