e5b69520931ac23fda88b5548b2afaec1eb839b7
Josphat karanja <josphatranja243@gmail.com>
2023-01-16 20:57:01 +0300

    Express PlaceHolders

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const { products } = require('./data');
 
-//SETUP STATIC AND MIDDLEWARE(static assets)
-app.use(express.static('./public'))
+app.get('/', (req, res) => {
+    res.send(
+        '<h1>Home Page</h1><a href="/api/products">Products</a>'
+    )
+})
 
-//app.get('/', (req, res) => {
-//1.Send index.html
-//res.sendFile(path.resolve(__dirname, './navBar/index.html'))
-//2.Just dump index.html in the static assests(./public folder)
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product;
+        return { id, name, image }
+    })
+    res.json(newProducts)
+})
 
-//})
-//3.SSR--Server-Side-Rendering
-
-app.all('*', (req, res) => {
-    res.status(404)
-        .send('Resource not Found')
+app.get('/api/products/:productID', (req, res) => {
+    const { productID } = req.params;
+    const singleProduct = products.find((product) => product.id === Number(productID))
+    if (!singleProduct) {
+        return res.status(404).send('Product Does Not Exist')
+    }
+    res.json(singleProduct)
 })
 
 app.listen(5000, () => {
-    console.log("Server is listening on port 5000...");
-})
+    console.log('Server is listening on port 5000....')
+}) 
