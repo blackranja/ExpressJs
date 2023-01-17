ab0c0ee54dd22ad1f8498e3a52842b3e930af3c9
Josphat karanja <josphatranja243@gmail.com>
2023-01-17 08:04:37 +0300

    Express and MiddleWare

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data');
-
+const logger = require('./logger');
+app.use(logger);
 app.get('/', (req, res) => {
-    res.send(
-        '<h1>Home Page</h1><a href="/api/products">Products</a>'
-    )
+    res.send('Home');
+})
+app.get('/about', (req, res) => {
+    res.send('About');
+})
+app.get('/contact', (req, res) => {
+    res.send('contact');
+})
+app.get('/people', (req, res) => {
+    res.send('people');
 })
 
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const { id, name, image } = product;
-        return { id, name, image }
-    })
-    res.json(newProducts)
-})
-
-app.get('/api/products/:productID', (req, res) => {
-    const { productID } = req.params;
-    const singleProduct = products.find((product) => product.id === Number(productID))
-    if (!singleProduct) {
-        return res.status(404).send('Product Does Not Exist')
-    }
-    res.json(singleProduct)
-})
 
 app.listen(5000, () => {
-    console.log('Server is listening on port 5000....')
-}) 
+    console.log('Server is Listening on port 5000....');
+})
