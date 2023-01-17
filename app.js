93371d4035e97b2358cccac1e16b51bc7f6c59f4
Josphat karanja <josphatranja243@gmail.com>
2023-01-17 20:11:23 +0300

    MiddleWare Morgan

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const app = express();
-const logger = require('./logger');
-app.use(logger);
-app.get('/', (req, res) => {
-    res.send('Home');
-})
-app.get('/about', (req, res) => {
-    res.send('About');
-})
-app.get('/contact', (req, res) => {
-    res.send('contact');
-})
-app.get('/people', (req, res) => {
-    res.send('people');
-})
+
 
 
 app.listen(5000, () => {
-    console.log('Server is Listening on port 5000....');
+    console.log(
+        'Server is listening on port 5000...'
+    )
 })
